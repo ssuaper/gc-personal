7be836b4cd904a4b6e792cdfb0aba6bdd82f280e
ssuaper <ssuaper@gobiernodecanarias.org>
2019-11-28 15:01:16 +0100

    Add initial project

---
+++ gc-personal-domain/src/main/java/gc/personal/PersonaDTO.java
@@ -0,0 +1,16 @@
+package gc.personal;
+
+/**
+ *
+ * @author docker
+ */
+public class PersonaDTO {
+    private Integer id;
+    private String name;
+    
+    public PersonaDTO() {
+        this.id = null;
+        this.name = null;
+    }
+    
+}
---
+++ gc-personal-ejb/src/main/java/gc/personal/PersonaEJB.java
@@ -0,0 +1,14 @@
+package gc.personal;
+
+
+/**
+ *
+ * @author docker
+ */
+public class PersonaEJB {
+    
+    public PersonaDTO buscarPersonaPorNombre() {
+        return new PersonaDTO();
+    }
+    
+}
